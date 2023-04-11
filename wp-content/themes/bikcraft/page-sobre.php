bde8552e839bd6a45e8491ea944766088f74ee6a
d1av <davi4alves@gmail.com>
2023-04-11 20:48:23 -0300

    feat: :sparkles:  add about component

--- wp-content/themes/bikcraft/page-sobre.php
+++ wp-content/themes/bikcraft/page-sobre.php
@@ -4,9 +4,7 @@ get_header();
 ?>
 
 
-<?php if (have_posts()) : while (have_posts()) : the_post();
-global $post;
-?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<?php include(TEMPLATEPATH . '/inc/introducao.php'); ?>
 
@@ -27,35 +25,12 @@ global $post;
 			</div>
 
 			<div class="grid-16 foto-equipe">
-				<img src="img/equipe-bikcraft.jpg" alt="Equipe Bikcraft">
+				<img src="<?php echo get_template_directory_uri() ?>/img/equipe-bikcraft.jpg" alt="Equipe Bikcraft">
 			</div>
 
 		</section>
 
-		<section class="qualidade container">
-			<h2 class="subtitulo">Qualidade</h2>
-			<img src="img/bikcraft-qualidade.png" alt="Bikcraft">
-			<ul class="qualidade_lista">
-				<li class="grid-1-3">
-					<h3>Durabilidade</h3>
-					<p>Sólida como pedra, leve como o vento e resistente como o diamante, são nossos diferenciais.</p>
-				</li>
-				<li class="grid-1-3">
-					<h3>Design</h3>
-					<p>Feitas sob medida para o melhor conforto e eficiência. Adaptamos a sua Bikcraft para o seu corpo.</p>
-				</li>
-				<li class="grid-1-3">
-					<h3>Sustentabilidade</h3>
-					<p>Além de ajudar a cuidar do meio ambiente, tirando carros da rua, toda a produção é sustentável.</p>
-				</li>
-			</ul>
-		</section>
-		<pre>
-<?php
-echo json_encode(pods('sobre', $post->ID), JSON_PRETTY_PRINT);
-echo pods('sobre', $post->ID)->template('teste');
- ?>
- </pre>
+		<?php include(TEMPLATEPATH . '/inc/qualidade.php') ?>
 <?php endwhile;
 else : endif; ?>
 
