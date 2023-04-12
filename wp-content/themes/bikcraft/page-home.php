224a22441dced47c219b4c4936a63f9352acb113
d1av <davi4alves@gmail.com>
2023-04-12 00:36:02 -0300

    feat: :sparkles:  add image cropping when upload

--- wp-content/themes/bikcraft/page-home.php
+++ wp-content/themes/bikcraft/page-home.php
@@ -3,55 +3,75 @@
 get_header();
 ?>
 
-<section class="introducao">
-	<div class="container">
-		<h1><?php the_field('titulo_introducao') ?></h1>
-		<blockquote class="quote-externo">
-			<p><?php the_field('quote_introducao') ?></p>
-			<cite><?php the_field('citacao_introducao') ?></cite>
-		</blockquote>
-		<a href="/produtos/" class="btn">Orçamento</a>
-	</div>
-</section>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<section class="produtos container animar">
-	<h2 class="subtitulo">Produtos</h2>
-	<ul class="produtos_lista">
+		<?php
+		$imagem_id = get_field('background_home');
+		$background_large = wp_get_attachment_image_src($imagem_id, 'large');
+		$background_medium = wp_get_attachment_image_src($imagem_id, 'medium');
+		?>
 
-		<li class="grid-1-3">
-			<div class="produtos_icone">
-				<img src="<?php echo get_template_directory_uri() ?>/img/produtos/passeio.png" alt="Bikcraft Passeio">
+		<style type="text/css">
+			.introducao {
+				background: url('<?php echo $background_large[0] ?>') no-repeat center;
+			}
+
+			@media only screen and (max-width: 767px) {
+				.introducao {
+					background: url('<?php echo $background_medium[0] ?>') no-repeat center;
+				}
+			}
+		</style>
+		<section class="introducao">
+			<div class="container">
+				<h1><?php the_field('titulo_introducao') ?></h1>
+				<blockquote class="quote-externo">
+					<p><?php the_field('quote_introducao') ?></p>
+					<cite><?php the_field('citacao_introducao') ?></cite>
+				</blockquote>
+				<a href="/produtos/" class="btn">Orçamento</a>
 			</div>
-			<h3>Passeio</h3>
-			<p>Muito melhor do que passear pela orla a vidros fechados.</p>
-		</li>
+		</section>
 
-		<li class="grid-1-3">
-			<div class="produtos_icone">
-				<img src="<?php echo get_template_directory_uri() ?>/img/produtos/esporte.png" alt="Bikcraft Esporte">
+		<section class="produtos container animar">
+			<h2 class="subtitulo">Produtos</h2>
+			<ul class="produtos_lista">
+
+				<li class="grid-1-3">
+					<div class="produtos_icone">
+						<img src="<?php echo get_template_directory_uri() ?>/img/produtos/passeio.png" alt="Bikcraft Passeio">
+					</div>
+					<h3>Passeio</h3>
+					<p>Muito melhor do que passear pela orla a vidros fechados.</p>
+				</li>
+
+				<li class="grid-1-3">
+					<div class="produtos_icone">
+						<img src="<?php echo get_template_directory_uri() ?>/img/produtos/esporte.png" alt="Bikcraft Esporte">
+					</div>
+					<h3>Esporte</h3>
+					<p>Mais rápida do que Forrest Gump, ninguém vai pegar você.</p>
+				</li>
+
+				<li class="grid-1-3">
+					<div class="produtos_icone">
+						<img src="<?php echo get_template_directory_uri() ?>/img/produtos/retro.png" alt="Bikcraft Retrô">
+					</div>
+					<h3>Retrô</h3>
+					<p>O passado volta para lembrarmos o que devemos fazer no futuro.</p>
+				</li>
+
+			</ul>
+
+			<div class="call">
+				<p><?php the_field('chamada_produtos') ?></p>
+				<a href="/produtos/" class="btn btn-preto">Produtos</a>
 			</div>
-			<h3>Esporte</h3>
-			<p>Mais rápida do que Forrest Gump, ninguém vai pegar você.</p>
-		</li>
 
-		<li class="grid-1-3">
-			<div class="produtos_icone">
-				<img src="<?php echo get_template_directory_uri() ?>/img/produtos/retro.png" alt="Bikcraft Retrô">
-			</div>
-			<h3>Retrô</h3>
-			<p>O passado volta para lembrarmos o que devemos fazer no futuro.</p>
-		</li>
-
-	</ul>
-
-	<div class="call">
-		<p><?php the_field('chamada_produtos') ?></p>
-		<a href="/produtos/" class="btn btn-preto">Produtos</a>
-	</div>
-
-</section>
-<!-- Fecha Produtos -->
-
+		</section>
+		<!-- Fecha Produtos -->
+<?php endwhile;
+else : endif; ?>
 <section class="portfolio">
 	<div class="container">
 		<h2 class="subtitulo">Portfólio</h2>
