7b85a12f1e09f989ecc48a3e9c87263551d10484
d1av <davi4alves@gmail.com>
2023-04-13 01:20:06 -0300

    feat: :sparkles:  increase performance

--- wp-content/themes/bikcraft/footer.php
+++ wp-content/themes/bikcraft/footer.php
@@ -1,10 +1,10 @@
 <?php $destiny = get_template_directory_uri(); ?>
-<div class="quebra">
+<!-- <div class="quebra">
     <blockquote class="quote-externo container">
         <p>“o verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana.”</p>
         <cite>WILLIAM MORRIS</cite>
     </blockquote>
-</div>
+</div> -->
 <footer>
     <div class="footer">
         <div class="container">
--- wp-content/themes/bikcraft/index.php
+++ wp-content/themes/bikcraft/index.php
@@ -1,11 +1,30 @@
-<?php
-get_header();
-?>
+<?php get_header(); ?>
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<h1><?php the_title(); ?></h1>
-		<?php the_content(); ?>
+
+		<article>
+			<section class="introducao-interna introducao-geral">
+				<div class="container">
+					<h1><?php the_title(); ?></h1>
+				</div>
+			</section>
+
+			<section class="container conteudo-geral">
+				<div class="grid-8">
+					<?php the_content(); ?>
+				</div>
+			</section>
+		</article>
+
 	<?php endwhile;
 else : ?>
-	<p><?php _e('Nenhum post foi encontrado com o critério.') ?></p>
+
+	<section class="introducao-interna introducao-geral">
+		<div class="container">
+			<h1>Página não encontrada.</h1>
+		</div>
+	</section>
+
 <?php endif; ?>
+
 <?php get_footer(); ?>
